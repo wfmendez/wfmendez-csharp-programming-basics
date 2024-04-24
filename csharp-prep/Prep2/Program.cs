6609ffc2c43ee0e9e8b3426f1dff7af54dcb7988
wfmendez <wuillian.f.mendez@gmail.com>
2024-04-24 08:45:38 -0400

    Finish C# - Prep2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,62 @@
 using System;
+using System.Reflection.Metadata;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string letter;
+        string sign = "";
+
+        Console.WriteLine("Hello, please type your grade:");
+        int grade = int.Parse(Console.ReadLine());
+        
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+
+        else
+        {
+            letter = "F";
+        }
+
+        int lastDigit = grade / 10;
+
+        if (lastDigit >= 7)
+        {
+            sign = "+";
+        }
+        
+        else if (lastDigit <= 3)
+        {
+            sign = "-";
+        }
+
+        Console.WriteLine($"Your grade is {letter}{sign}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! Your pass the semester.");
+        }
+        else {
+            Console.WriteLine("Sorry, you don't pass the semester");
+        }
     }
 }
