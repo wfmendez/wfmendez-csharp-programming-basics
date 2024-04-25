c33009eebe7ff3ddc2d17e60e59d969d188b9269
wfmendez <wuillian.f.mendez@gmail.com>
2024-04-25 10:28:06 -0400

    Finish C# Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        Console.WriteLine("What is your guess?");
+        int guessNumber = int.Parse(Console.ReadLine());
+        int attemps = 1;
+        int endGame = 0;
+
+        while (endGame == 0)
+        {
+            while (magicNumber != guessNumber)
+            {
+
+                if (guessNumber < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+
+                else if (guessNumber > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+
+                attemps += 1;
+
+                Console.WriteLine("What is your guess?");
+                guessNumber = int.Parse(Console.ReadLine());
+            }
+
+            Console.WriteLine("You guessed it!");
+            Console.WriteLine($"Total guess attemps: {attemps}");
+
+            Console.WriteLine("Do you want to play again? YES/NO");
+            string playAgain = Console.ReadLine();
+            
+            if (playAgain == "YES")
+            {
+                magicNumber = randomGenerator.Next(1, 100);
+            }
+            else if (playAgain == "NO")
+            {
+                endGame = 1;
+            }
+        }
+        Console.WriteLine("END GAME");
     }
+
 }
