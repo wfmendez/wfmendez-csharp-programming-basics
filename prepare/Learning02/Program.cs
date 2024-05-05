2d04ae8e92d12cefd868aa0c31e6cfb082f2d2b2
wfmendez <wuillian.f.mendez@gmail.com>
2024-05-04 21:44:55 -0400

    Finish Learning 2

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -1,9 +1,35 @@
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        // JOB 1
+        Job job1 = new Job();
+
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2019;
+        job1._endYear = 2022;
+
+        job1.Display();
+
+        // JOB 2
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2022;
+        job2._endYear = 2023;
+
+        job2.Display();
+
+        // MY RESUME
+        Resume myResume = new Resume();
+        myResume._name = "Allison Rose";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
---
+++ prepare/Learning02/Resume.cs
@@ -0,0 +1,19 @@
+using System;
+
+public class Resume
+{
+    public string _name = "";
+
+    public List<Job> _jobs = new List<Job>();
+
+    public void Display()
+    {
+        Console.WriteLine($"Name: {_name}");
+        Console.WriteLine("Jobs:");
+
+        foreach (Job j in _jobs)
+        {
+            j.Display();
+        }
+    }
+}
